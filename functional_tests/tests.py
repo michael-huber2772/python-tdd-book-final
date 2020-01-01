7acd4e21aca5b4eaa04aad6bf9823655f247ca56
michael-huber2772 <44558743+michael-huber2772@users.noreply.github.com>
2020-01-01 16:06:28 -0700

    End of chapter 7

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
 import time
 
 MAX_WAIT = 10
+
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -13,6 +14,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -26,6 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -64,13 +67,8 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-
-        # She visits that URL - her to-do list is still there.
-
         # Satisfied, she goes back to sleep
+
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
@@ -84,7 +82,7 @@ class NewVisitorTest(LiveServerTestCase):
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
 
-         # Now a new user, Francis, comes along to the site.
+        # Now a new user, Francis, comes along to the site.
 
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
